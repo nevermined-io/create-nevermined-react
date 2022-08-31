2397abe7318a021e462d43bf7732636cbbaaf88a
spieljs <spieljs@gmail.com>
2022-08-31 13:28:02 +0200

    improve logs and fix download artifacts

--- .bin/create-nevermined-react.js
+++ .bin/create-nevermined-react.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { spawn } = require("child_process");
-const fs   = require("fs");
+const fs = require("fs");
 
 let folderName = '.';
 
@@ -11,40 +11,57 @@ if (process.argv.length >= 3) {
   }
 }
 
+const handleError = (type, errCode) => {
+  console.error(`Error type: ${type}, code: ${errCode}`)
+  process.exit(errCode);
+}
+
+const logs = (name, process) => {
+  process.stdout.setEncoding('utf8');
+  process.stdout.on('data', (data) => {
+    console.info(`${name}: ${data}`);
+  });
+  process.stderr.setEncoding('utf8');
+  process.stderr.on('data', (data) => {
+    console.warn(`${name}: ${data}`);
+  });
+};
+
 const clone = spawn("git", ["clone", "git@github.com:nevermined-io/create-nevermined-react.git", folderName]);
+
+logs('git clone', clone);
 
 clone.on("close", (code) => {
   if (code !== 0) {
     handleError("install", code);
   } else {
-    console.log(" Nevermined repository is cloned. Installing dependencies... ");
+    console.info(" Nevermined repository is cloned. Installing dependencies... ");
 
     const install = spawn('npm', ['install'], { cwd: folderName });
+
+    logs('install', install);
 
     install.on("close", (code) => {
       if (code !== 0) {
         handleError("install", code);
       } else {
-        console.log(" Installed dependencies ✅ ");
-        console.log(" Downloading artifacts... ");
+        console.info(" Installed dependencies ✅ ");
+        console.info(" Downloading artifacts... ");
 
-        const artifacts = spawn('npm', ['run', 'artifacts:download'], { cwd: folderName });
+        const artifacts = spawn('sh', ['./scripts/download-artifacts.sh', 'v2.0.5', 'mumbai'], { cwd: folderName });
+
+        logs('artifacts', artifacts);
   
         artifacts.on("close", (code) => {
           if (code !== 0) {
               handleError("artifacts", code);
           }
-          console.log(" Artifacts downloaded ")
-          console.log(" Now you can start your dApp with 'npm start' and enjoy!! ");
-          console.log(" Don't forget to visit us in our Discord https://discord.com/invite/GZju2qScKq ");
-          console.log(" Nevermined Team ")
+          console.info(" Artifacts downloaded ")
+          console.info(" Now you can start your dApp with 'npm start' and enjoy!! ");
+          console.info(" Don't forget to visit us in our Discord https://discord.com/invite/GZju2qScKq ");
+          console.info(" Nevermined Team ")
         })
       }
     });
   }
 });
-
-function handleError(type, errCode) {
-    console.error(`Error type: ${type}, code: ${code}`)
-    process.exit(errCode);
-}
